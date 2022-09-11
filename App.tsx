0a87736425ff9919fcc5d9834d0f6079129e2585
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 10:22:18 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import CustomerScreen from "./screens/CustomerScreen";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RootNavigator from "./navigator/RootNavigator";
 
 export default function App() {
 
@@ -11,9 +12,7 @@ export default function App() {
     return (
         // @ts-ignore
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Customer" component={CustomerScreen} />
-            </Stack.Navigator>
+            <RootNavigator />
             <StatusBar style="auto" />
         </NavigationContainer>
   );
--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomerScreen from "../screens/CustomerScreen";
 import OrdersScreen from "../screens/OrdersScreen";
+import {useNavigation} from "@react-navigation/native";
 
-const Tab = createBottomTabNavigator();
+
+export type TabStackParamList = {
+    Customers: undefined;
+    Orders: undefined;
+}
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
+    const navigation = useNavigation();
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, []);
+
+
     return (
         <Tab.Navigator>
             <Tab.Screen name="Customers" component={CustomerScreen} />
