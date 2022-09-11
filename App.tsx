6326214f9ccee157fb004f54b2d58b4d11ab6c0d
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 09:58:04 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomerScreen from "./screens/CustomerScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    return (
+  // @ts-ignore
+    <View className="">
+        <CustomerScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/CustomerScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+
+const CustomerScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-red-500 font-bold">
+                byrookas 🚀
+            </Text>
+        </SafeAreaView>
+    );
+};
+
+export default CustomerScreen;
