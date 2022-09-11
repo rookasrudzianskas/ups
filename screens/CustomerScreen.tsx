3a0f110d9f2cde97d8cf17b244725d04d2056f42
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 10:41:51 +0300

    Rokas

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {Text, View, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import {CompositeNavigationProp, useNavigation} from "@react-navigation/native";
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
+import {TabStackParamList} from "../navigator/TabNavigator";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../navigator/RootNavigator";
+
+export type CustomerScreenNavigationProp =
+    CompositeNavigationProp<
+    BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+    NativeStackNavigationProp<RootStackParamList>
+    >
 
 const CustomerScreen = () => {
+    const navigation = useNavigation<CustomerScreenNavigationProp>()
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            // hide header
+            headerShown: false
+        })
+    })
     return (
-        <SafeAreaView>
-            {/* @ts-ignore */}
-            <Text className="text-red-500 font-bold">
-                byrookas 🚀
-            </Text>
-        </SafeAreaView>
+        <ScrollView>
+
+        </ScrollView>
     );
 };
 
