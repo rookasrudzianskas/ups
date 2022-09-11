1b3f083450a6e4088c4da160ccd653bfdf967ea3
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 15:25:05 +0300

    Rokas

--- components/CustomerCard.tsx
+++ components/CustomerCard.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import useCustomerOrders from "../hooks/useCustomerOrders";
 import {useNavigation} from "@react-navigation/native";
-import {Card} from "@rneui/themed";
+import {Card, Icon} from "@rneui/themed";
 import {useTailwind} from "tailwind-rn/dist";
+import {CardDivider} from "@rneui/base/dist/Card/Card.Divider";
 
 type Props = {
     userId: string;
@@ -24,9 +25,31 @@ const CustomerCard = ({email, name, userId}: Props) => {
                     padding: 20
                 }}>
                     <View>
+                        {/* @ts-ignore */}
+                        <View className="flex flex-row justify-between">
+                            <View>
+                                {/* @ts-ignore */}
+                                <Text className="text-2xl font-bold">{name}</Text>
+                                {/* @ts-ignore */}
+                                <Text className="text-sm" style={{color: '#59C1CC'}}>ID: {userId}</Text>
+                            </View>
 
+                        {/*     @ts-ignore */}
+                        <View className="flex flex-row items-center justify-end">
+                            <Text style={{color: '#59C1CC'}}>{loading ? "loading..." : `${orders.length} x`}</Text>
+                            <Icon
+                                name="box"
+                                size={50}
+                                // @ts-ignore
+                                type={'entypo'}
+                                color={'#59C1CC'}
+                                style={{marginBottom: 20, margin: 'auto'}}
+                            />
+                        </View>
+                        </View>
                     </View>
-
+                    <CardDivider />
+                    <Text>{email}</Text>
                 </Card>
         </TouchableOpacity>
     );
