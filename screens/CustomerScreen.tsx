46353f618d388480572f44ae08cc873b0fa83260
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 11:54:45 +0300

    Rokas

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,10 +1,13 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import {Text, View, StyleSheet, SafeAreaView, ScrollView, Image} from 'react-native';
 import {CompositeNavigationProp, useNavigation} from "@react-navigation/native";
 import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
 import {TabStackParamList} from "../navigator/TabNavigator";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../navigator/RootNavigator";
+import {Input} from "@rneui/themed";
+import {useTailwind} from 'tailwind-rn';
+
 
 export type CustomerScreenNavigationProp =
     CompositeNavigationProp<
@@ -13,17 +16,28 @@ export type CustomerScreenNavigationProp =
     >
 
 const CustomerScreen = () => {
-    const navigation = useNavigation<CustomerScreenNavigationProp>()
+    const navigation = useNavigation<CustomerScreenNavigationProp>();
+    const [input, setInput] = useState<string>('');
+    const tw = useTailwind();
+
+
     useLayoutEffect(() => {
         navigation.setOptions({
             // hide header
             headerShown: false
         })
     })
+    // @ts-ignore
     return (
-        <ScrollView>
+        // @ts-ignore
+        <ScrollView className="bg-[#59C1CC]">
             {/* @ts-ignore */}
             <Image source={{uri: 'https://links.papareact.com/3jc'}} className="w-full h-64" />
+
+            {/* @ts-ignore */}
+            <View className="bg-white pt-5 pb-0 px-10">
+                <Input placeholder="Search by Customer" value={input} onChangeText={(text) => setInput(text)} />
+            </View>
         </ScrollView>
     );
 };
