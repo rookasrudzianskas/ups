90ccc9c8df5d2d5776ac18a4a0e6a95ed1d38273
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 13:58:01 +0300

    Rokas

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -11,3 +11,34 @@ export const GET_CUSTOMERS = gql`
         }
     }
 `;
+
+
+export const GET_ORDERS = gql`
+    query GetOrders {
+        getOrders {
+            value {
+                carrier
+                createdAt
+                trackingId
+                shippingCost
+                Address
+                City
+                Lng
+                Lat
+                trackingItems {
+                    customer_id
+                    customer {
+                        email
+                        name
+                    }
+                    items {
+                        item_id
+                        name
+                        price
+                        quantity
+                    }
+                }
+            }
+        }
+    }
+`;
