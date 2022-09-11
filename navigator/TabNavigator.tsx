559006907c7d4af009608b63479209b1a32218a1
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 10:16:31 +0300

    Rokas

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CustomerScreen from "../screens/CustomerScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Customers" component={CustomerScreen} />
+            <Tab.Screen name="Orders" component={OrdersScreen} />
+        </Tab.Navigator>
     );
 };
 
---
+++ screens/OrdersScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const OrdersScreen = () => {
+    return (
+        <View>
+            <Text>
+                Order
+            </Text>
+        </View>
+    );
+};
+
+export default OrdersScreen;
