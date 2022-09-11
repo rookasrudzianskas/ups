9583e5a839c9f570d93e70f943c99c6e3f2fb261
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 15:41:56 +0300

    Rokas

--- components/ModalScreen.tsx
+++ components/ModalScreen.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Icon} from "@rneui/themed";
-import {useNavigation} from "@react-navigation/native";
+import {CompositeNavigationProp, RouteProp, useNavigation, useRoute} from "@react-navigation/native";
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
+import {TabStackParamList} from "../navigator/TabNavigator";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../navigator/RootNavigator";
+
+type ModalScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList>, NativeStackNavigationProp<RootStackParamList, "MyModal">>;
+
+type ModalScreenRouteProp = RouteProp<RootStackParamList, 'MyModal'>
 
 const ModalScreen = () => {
     const navigation = useNavigation();
+    const {params: {name, userId}} = useRoute<ModalScreenRouteProp>();
     return (
         <View>
     {/*// @ts-ignore*/}
             <TouchableOpacity onPress={() => navigation.goBack()} className="absolute top-5 right-5 z-10">
                 <Icon name="closecircle" type="antdesign" />
             </TouchableOpacity>
+
+            <View>
+                <View>
+                    <Text>{name}</Text>
+                    <Text>deliveries</Text>
+                </View>
+            </View>
         </View>
     );
 };
