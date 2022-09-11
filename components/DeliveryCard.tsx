baedb83d808e8ecdd89ab852801a350930bcaf74
Rokas <rookas.rudzenskas@gmail.com>
2022-09-11 15:47:59 +0300

    Rokas

---
+++ components/DeliveryCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+type Props = {
+    order: Order
+}
+
+const DeliveryCard = ({order}: Props) => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default DeliveryCard;
--- components/ModalScreen.tsx
+++ components/ModalScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import {Icon} from "@rneui/themed";
 import {CompositeNavigationProp, RouteProp, useNavigation, useRoute} from "@react-navigation/native";
 import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
 import {TabStackParamList} from "../navigator/TabNavigator";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../navigator/RootNavigator";
+import useCustomerOrders from "../hooks/useCustomerOrders";
+import DeliveryCard from "./DeliveryCard";
 
 type ModalScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList>, NativeStackNavigationProp<RootStackParamList, "MyModal">>;
 
@@ -14,6 +16,8 @@ type ModalScreenRouteProp = RouteProp<RootStackParamList, 'MyModal'>
 const ModalScreen = () => {
     const navigation = useNavigation();
     const {params: {name, userId}} = useRoute<ModalScreenRouteProp>();
+    const {loading, error, orders } = useCustomerOrders(userId);
+
     return (
         <View>
     {/*// @ts-ignore*/}
@@ -21,12 +25,17 @@ const ModalScreen = () => {
                 <Icon name="closecircle" type="antdesign" />
             </TouchableOpacity>
 
-            <View>
-                <View>
-                    <Text>{name}</Text>
-                    <Text>deliveries</Text>
+            <View style={{marginTop: 10}}>
+                {/* @ts-ignore */}
+                <View className="py-5 border-b " style={{borderColor: '#59C1CC'}}>
+                    {/* @ts-ignore*/}
+                    <Text className="text-center text-xl font-bold" style={{color: '#59C1CC'}}>{name}</Text>
+                    {/* @ts-ignore*/}
+                    <Text className="text-center italic text-sm">deliveries</Text>
                 </View>
             </View>
+
+            <FlatList data={orders} keyExtractor={order => order.trackingId} renderItem={({item: order}) => <DeliveryCard order={order}/>} />
         </View>
     );
 };
